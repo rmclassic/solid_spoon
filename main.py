e9db611002ed58ee6a2f5eedceaa4cf26ed76ab0
Ramin Khonsari <ramin.khoonsai@sjicompany.com>
2022-07-05 15:18:55 +0430

    implement similarity calculation using fixed-size word window

--- main.py
+++ main.py
@@ -2,49 +2,97 @@ from sklearn.datasets import fetch_20newsgroups
 import sklearn.datasets
 from nltk.tokenize import word_tokenize
 import nltk
+import util
+import json
 from window import getWindowJaccard
 
 newsgroups_train = fetch_20newsgroups(subset='train')
 
 data_tokens = []
 documents = []
-for d in newsgroups_train.data:
-  # data_tokens.append(word_tokenize(d))
-  sentences = nltk.sent_tokenize(d)
-  documents.append(sentences)
-
-
-sims = {}
-for i in range(200):#range(len(documents)):
-    d = documents[i]
-    #if i % 100 == 0:
-    print('processed ', i, '/', len(documents),' documents')
-
-    jacs = getWindowJaccard(d)
-    for jac in jacs:
-        if jac['jaccard'] > 0.5:
-            if jac['w1'] in sims:
-                if jac['w1'] != jac['w2']:
-                    sims[jac['w1']].append(jac['w2'])
-            else:
-                sims[jac['w1']] = [jac['w2'],]
-
-print(sims)
+# for d in newsgroups_train.data:
+#   # data_tokens.append(word_tokenize(d))
+#   sentences = nltk.sent_tokenize(d)
+#   documents.append(sentences)
 #
-# for i in range(len(data_tokens)):
-#     data_tokens[i] = list(filter(lambda l:len(l) >= 3 , list(map(lambda x:x.lower(), data_tokens[i]))))
-
-# uniques = {}
-# for i in range(len(data_tokens)):
-#     for word in data_tokens[i]:
-#         if (not uniques.get(word)):
-#             uniques[word] = 1
-#         else:
-#             uniques[word] += 1
-
-
-
-# print(uniques)
 #
-# uniques = dict(sorted(uniques.items(), key=lambda item: item[1]))
-# print(uniques)
+# sims = {}
+# for i in range(200):#range(len(documents)):
+#     d = documents[i]
+#     #if i % 100 == 0:
+#     print('processed ', i, '/', len(documents),' documents')
+#
+#     jacs = getWindowJaccard(d)
+#     for jac in jacs:
+#         if jac['jaccard'] > 0.5:
+#             if jac['w1'] in sims:
+#                 if jac['w1'] != jac['w2']:
+#                     sims[jac['w1']].append(jac['w2'])
+#             else:
+#                 sims[jac['w1']] = [jac['w2'],]
+#
+# print(sims)
+
+def save_state(data, tag):
+    dstr = json.dumps(data)
+    f = open(tag + '.state', 'w')
+    f.write(dstr)
+    f.close()
+
+def load_state(tag):
+    try:
+        f = open(tag + '.state', 'r')
+        dstr = f.read()
+        f.close()
+        data = json.loads(dstr)
+    except:
+        return None
+    return data
+
+
+data = load_state('preprocess')
+if data == None:
+    data = newsgroups_train.data[:2000]
+    for i, doc in enumerate(data):
+        data[i] = util.preprocess_document(doc)
+
+    save_state(data, 'preprocess')
+else:
+    print('preprocess: using previous state')
+
+base_border = util.getCollectionBorder(data)
+
+word_borders = load_state('border_generation')
+
+if word_borders == None:
+    word_borders = []
+    for i, word in enumerate(base_border):
+        print('generating word border for \'' + word + '\':' , i, '/', len(base_border))
+        w_border = util.getWordBorder(base_border, word, data)
+        print({'word': word, 'border': w_border})
+        word_borders.append({'word': word, 'border': w_border})
+
+    save_state(word_borders, 'border_generation')
+else:
+    print('border_generation: using previous state')
+
+print('calculating similarities')
+
+similarities = load_state('similarity_calculation')
+
+if similarities == None:
+    similarities = []
+    for i, w1_border in enumerate(word_borders):
+        for j, w2_border in enumerate(word_borders[i + 1:]):
+            w_sim = util.calculateBorderSimilarity(w1_border, w2_border)
+
+            if w_sim != 0 and w_sim > 0.1:
+                similarities.append({'w1': w1_border['word'], 'w2': w2_border['word'], 'similarity': w_sim})
+                print({'w1': w1_border['word'], 'w2': w2_border['word'], 'similarity': w_sim})
+    save_state(similarities, 'similarity_calculation')
+
+else:
+    print('similarity_calculation: using previous state')
+
+for s in similarities:
+    print(s)
---
+++ util.py
@@ -0,0 +1,85 @@
+import nltk
+import math
+from nltk.corpus import stopwords
+nltk.download('punkt')
+nltk.download('stopwords')
+sw = stopwords.words('english')
+
+WINDOW_RADIUS = 2
+
+def preprocess_document(doc):
+    doc = nltk.word_tokenize(doc)
+    processed = []
+    processed = list(filter(lambda l:len(l) >= 3 and l.isalpha() and l not in sw, list(map(lambda x:x.lower(), doc))))
+    return processed
+
+# def preprocess_document_sent(doc):
+#     doc = nltk.sent_tokenize(doc)
+#     return doc
+
+def getCollectionBorder(col):
+    border = {}
+    for index, doc in enumerate(col):
+        if index % 100 == 0:
+            print('processed', index, 'docs')
+
+        for w in doc:
+            if w not in border:
+                border[w] = 0
+    return border
+
+def makeWindowAround(index, doc):
+    start_index = index if index - WINDOW_RADIUS > 0 else 0
+    end_index = index + WINDOW_RADIUS if index + WINDOW_RADIUS < len(doc) else len(doc) - 1
+    return doc[start_index:end_index]
+
+def getWordWindows(word, doc):
+    last_index = 0
+    windows = []
+
+    while last_index != -1 and last_index + 1 < len(doc):
+        try:
+            last_index = doc.index(word, last_index + 1)
+        except:
+            last_index = -1
+            continue
+
+        window = makeWindowAround(last_index, doc)
+        windows.append(window)
+
+    #print(word)
+    #print(windows)
+    return windows
+
+def normalizeBorder(word, border):
+    try:
+        max_val = border[word]
+    except:
+        return {}
+    border[word] = 0
+    border.update((x, y/max_val) for x, y in border.items())
+    return border
+
+def getWordBorder(base_border, word, col):
+    border = {}
+    for doc in col:
+        w_wins = getWordWindows(word, doc)
+        for window in w_wins:
+            for neighbor in window:
+                if border.get(neighbor):
+                    border[neighbor] += 1
+                else:
+                    border[neighbor] = 1
+    return normalizeBorder(word, border)
+
+
+def calculateBorderSimilarity(w1_border, w2_border):
+    dot_sum = 0
+
+    for neighbor in w1_border['border']:
+        if not w2_border['border'].get(neighbor):
+            continue
+
+        dot_sum += w1_border['border'][neighbor] * w2_border['border'][neighbor] / math.sqrt(len(w1_border['border']) * len(w2_border['border']))
+
+    return dot_sum
